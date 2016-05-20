01662f04f1098190c128096f38ce573c0ae0d149
MdelM <misamuna@gmail.com>
2016-05-20 17:20:11 +0200

    Base project with drawer active

---
+++ app/src/main/java/com/synapsehub/viuc/fragment/UsersFragment.java
@@ -0,0 +1,26 @@
+package com.synapsehub.viuc.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.synapsehub.viuc.R;
+
+/**
+ * Created by Michelo on 13/01/16.
+ */
+public class UsersFragment extends Fragment {
+
+    public UsersFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
+    }
+}
